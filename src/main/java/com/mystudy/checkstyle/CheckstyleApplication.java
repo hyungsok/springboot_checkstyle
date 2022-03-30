30e2d221f4fd815da19d4de1ae5ac9320c3d233b
hyungsok lee <hyungsok7@gmail.com>
2022-03-30 18:30:30 +0900

    init

---
+++ src/main/java/com/mystudy/checkstyle/CheckstyleApplication.java
@@ -0,0 +1,21 @@
+package com.mystudy.checkstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class CheckstyleApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Google Java CodeStyle";
+    }
+
+    public static void main(final String[] args) {
+        SpringApplication.run(CheckstyleApplication.class, args);
+    }
+
+}
