d25d39c149a3c06d7730b775568a4ba95be88cf2
hyungsok lee <hyungsok7@gmail.com>
2022-03-31 11:40:10 +0900

    google checkstyle 변경

--- src/main/java/com/mystudy/checkstyle/CheckstyleApplication.java
+++ src/main/java/com/mystudy/checkstyle/CheckstyleApplication.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class CheckstyleApplication {
 
-    @RequestMapping("/")
+    @RequestMapping(
+        "/"
+    )
     String home() {
         return "Google Java CodeStyle";
     }
